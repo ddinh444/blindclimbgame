0976dccc94fe7841e5cfcc42d8aa7d1a6a6b3b62
Daniel Dinh <danieldinh474@gmail.com>
2026-04-07 08:48:02 -0500

    basic moving platform script added

---
+++ Assets/Scripts/Systems/MovingPlatforms/MovingPlatform.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]private bool loop;
+    [SerializeField]private Transform[] waypoints;
+    [SerializeField]private float speed;
+
+    private int index = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[index], Time.deltaTime * speed);
+        if(Vector3.Distance(transform.position, waypoints[index]) < 0.01f){
+            index += 1;
+            if(index > waypoints.Length){
+                index = 0;
+            }
+        }
+    }
+}
